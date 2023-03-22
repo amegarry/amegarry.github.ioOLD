ac1b0bda29ef4e3ef61263f47c464b2d78210b7c
amegarry <110860535+amegarry@users.noreply.github.com>
2023-03-22 16:33:59 -0500

    GP3

---
+++ JS/index.js
@@ -0,0 +1,21 @@
+console.log("your index.js file is loaded correctly!");
+
+console.log("your index.js file is loaded correctly!");
+$("button").hover(function(){
+    $(this).css("background-color","#487A6C");
+  },
+  function(){
+    $(this).css("background-color", "3B6D5F");
+  });
+
+  $("button").hover(function(){
+    $(this).css("background-color","#00FF00");
+  },
+  function(){
+
+    $(this).css("background-color", "#D3D3D3.");
+
+    $(this).css("background-color","#FFFFFF");
+
+  });
+
